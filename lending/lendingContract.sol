94b113e7b37939205e1dbab6eab2257e67911c60
akis <akis@ecoc.io>
2020-09-15 18:07:37 +0700

    implementing functions

--- lending/lendingContract.sol
+++ lending/lendingContract.sol
@@ -5,9 +5,10 @@ import "./lendingInterface.sol"
 contract lendingContract is lendingInterface {
 
 address owner;
-address oracles[];
-mapping (uint256 => uint32) private ecocRate;
-uint interestRate, collateralRate;
+mapping (address => bool) private oracles;
+mapping (string => mapping(uint256 => uint256)) private EFGRate;
+mapping (string => uint256) private InterestRate;
+uint collateralRate;
 
 constructor() {
     owner = msg.sender;
@@ -18,7 +19,7 @@ constructor() {
     * 8 decimal places
     * rate per block = annual rate * 1e8 / 985500
     */
-    interestRate = 1015 ;
+    interestRate['ECOC'] = 1015 ;
     
     /* Initial collateral rate is 25% , 2 decimal places. */
     collateralRate = 2500; /* 25% */
@@ -30,71 +31,88 @@ modifier ownerOnly() {
     }
 
 modifier oracleOnly() {
-    bool exists = false;
-    for (uint i =0; i < oracles.length; i++) {
-        if (oracles[i] == msg.sender) {
-            exists = true;
-            break;
-        }
-    }
-    require(exists);
+    require(oracle[msg.sender]);
         _;
     }
 
 modifier colletoralOffMargin()  {
+     require(msg.sender==owner);
+     /* implement the check for margin call below */
+     require(false);
         _;
     }
 /**
 * @notice add or purge oracles, only contract owner
-* @param oracle - the address of the oracle to be add or remove
-* @param action - a boolean , if true add to list; else remove (if oracle exists)
+* @param oracleAddr - the address of the oracle to be add or remove
+* @param action - a boolean , if true add to list; else unauthorize
 * @return a boolean , true on success
 */
-function authOracles(address oracle, bool action) public ownerOnly() returns (bool);
+function authOracles(address _oracleAddr, bool _action) public ownerOnly() returns (bool) {
+    oracles[_oracleAddr]=_action;
+    return true;
+}
 
 
 /**
 * @notice get exchange rate , 8 decimal places
+* @param symbol
 * @param timestamp
 * @return uint
 */
-function getEcocRate(uint timestamp) view returns (uint);
+function getEFGRates(string _symbol, uint _timestamp) view returns (uint) {
+    return EFGRate[_symbol][_timestamp];
+}
 
 /**
 * @notice set exchnage rate , 8 decimal places, only authorized oracle
+* @param symbol
 * @param timestamp 
 * @param rate
 * @return bool
 */
-function setEcocRate(uint rate, uint timestamp) public oracleOnly() returns (bool);
+function setEFGRate(string symbol, uint timestamp, uint rate) external oracleOnly() returns (bool){
+    EFGRate[_symbol][_timestamp] = _rate;
+    return true;
+}
 
 /**
-* @notice set interest rate , 4 decimal places, only contract owner
+* @notice set interest rate , 8 decimal places, only contract owner
+* @param _symbol
+* @param _interestRate
+* @return bool
+*/
+function setInterestRate(string _symbol, uint _interestRate) external ownerOnly() returns (bool) {
+    interestRate[_symbol] = _interestRate;
+    return true;
+}
+
+/**
+* @notice get interest rate, 8 decimal places
+* @param _symbol
+* @return uint - the interest rate of the asset
+*/
+function getInterestRate(string _symbol) view returns (uint) {
+    return interestRate[_symbol];
+}
+
+/**
+* @notice set interest rate , 8 decimal places, only contract owner
 * @param rate
 * @return bool
 */
-function setInterestRate(uint rate) public returns (bool);
+function setCollateralRate(uint _rate) public ownerOnly() returns (bool){
+    collateralRate = _rate;
+    return true;
+}
 
 /**
-* @notice get interest rate, 4 decimal places
+* @notice get interest rate, 8 decimal places
 * @param rate
 * @return bool
 */
-function getInterestRate() view returns (uint);
-
-/**
-* @notice set interest rate , 4 decimal places, only contract owner
-* @param rate
-* @return bool
-*/
-function setCollateralRate(uint rate) public ownerOnly() returns (bool);
-
-/**
-* @notice get interest rate, 4 decimal places
-* @param rate
-* @return bool
-*/
-function getCollateralRate() view returns (uint);
+function getCollateralRate() view returns (uint){
+    return collateralRate;
+}
 
 /**
 * @notice Deposit collateral
